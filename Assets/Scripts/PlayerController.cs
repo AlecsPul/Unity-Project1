0d9ba2d2a27739852bbb0a3b8db55a0c7b89fe0d
AlecsPul <alejandro.poole01@estudiant.upf.edu>
2026-01-30 23:54:22 +0100

    Added Collisions(a few). Changed The tilemap sprites and some decoration

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     public InputAction MoveAction;
     private void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
        
     }
     private void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>(); // Get the current value of the Move action
+        move = MoveAction.ReadValue<Vector2>(); // Get the current value of the Move action
         Debug.Log(move);
+    }
+    void FixedUpdate()
+    {
         Vector2 position = (Vector2)transform.position + move *3.0f * Time.deltaTime; //Time.deltaTime makes the character move the same units for all fps
         transform.position = position;
-
     }
 }
