52a44662d587e549600163e583c4985d563dab2b
AlecsPul <pdpdalld@gmail.com>
2026-02-18 23:47:52 +0100

    Healthbar current health display(change Healthbar sprite)

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,6 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class UIHandler : MonoBehaviour
 {
+   private VisualElement m_Healthbar;
     public float CurrentHealth = 0.5f;
+   void Start()
+   {
+        UIDocument uiDocument = GetComponent<UIDocument>();
+        m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBarBackground");
+        //SetHealthValue(0.2f);
+        m_Healthbar.style.width = Length.Percent(CurrentHealth*100.0f);
+   }
+
+
+   public void SetHealthValue(float percentage)
+   {
+       //m_Healthbar.style.width = Length.Percent(100 * percentage);
+   }
+
+
 }
