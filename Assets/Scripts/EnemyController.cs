3d907b4226c4c4e6dd62ac53650fc493f8beec8f
AlecsPul <pdpdalld@gmail.com>
2026-02-22 23:04:04 +0100

    Added enemy patrol movement(L pattern)

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public float changeTime;
+    private float timer;
+    private int patrolPhase = 0; // 0: right, 1: up, 2: left, 3: down
+    Rigidbody2D rigidbody2D;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        if(player != null)
+        {
+            player.ChangeHealth(-20);
+        }
+    }
+    void Start()
+    {
+        timer = changeTime;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            // Patrol following an L shape
+            if(patrolPhase == 0){
+                patrolPhase = 1;
+            }
+            else if(patrolPhase == 1){
+                patrolPhase = 3;
+            }
+            else if(patrolPhase == 2){
+                patrolPhase = 0;
+            }
+            else if(patrolPhase == 3){
+                patrolPhase = 2;
+            }
+            
+            timer = changeTime;
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+        switch (patrolPhase)
+        {
+            case 0: // Move right (x+)
+                position.x += speed * Time.deltaTime;
+                break;
+            case 1: // Move up (y+)
+                position.y += speed * Time.deltaTime;
+                break;
+            case 2: // Move left (x-)
+                position.x -= speed * Time.deltaTime;
+                break;
+            case 3: // Move down (y-)
+                position.y -= speed * Time.deltaTime;
+                break;
+        }
+        rigidbody2D.MovePosition(position);
+    }
+}
